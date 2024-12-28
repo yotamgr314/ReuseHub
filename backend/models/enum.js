0df584ab26bb71035abb302fd3f1f7ff81b07824
Rod_Yon <novakovskiy.ra@outlook.com>
2024-12-28 04:05:49 +0200

    Created 'enum.js' file
    The file 'enum.js' contains all enumerate type structures used in the schemes.

---
+++ backend/models/enum.js
@@ -0,0 +1,29 @@
+// All possible categories for the item specified in ad
+const categoryEnum = [
+    'Clothes',
+    'Furniture',
+    'Electronics',
+    'Kitchenware',
+    'Toys',
+    'Books',
+    'Sport Equipment',
+    'Cosmetics',
+    'Pet Supplies',
+    'Other'
+];
+
+// All possible conditions of an object, ranked from best to worst
+const conditionEnum = [
+    'New',
+    'Like New',
+    'Very Good',
+    'Good',
+    'Average',
+    'repaired',
+    'Used',
+    'Defective',
+    'Bad',
+    'Very Bad'
+];
+
+module.exports = { categoryEnum, conditionEnum };
