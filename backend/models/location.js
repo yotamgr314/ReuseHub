8465a5731313c2c68d5a52f69c41bf90f7a8fbc2
Rod_Yon <novakovskiy.ra@outlook.com>
2024-12-28 05:06:36 +0200

    Created 'location.js' file
    The file 'location.js' contains the schema for the location of the ad

---
+++ backend/models/location.js
@@ -0,0 +1,17 @@
+// Import of necessary modules
+const { Schema, model, Types } = require('mongoose');
+
+
+// Location schema storing 2 coordinates: x-axis and y-axis
+const locationSchema = new Schema({
+
+    // X-axis coordinate
+    x: { type: Number, requred: true },
+
+    // Y-axis coordinate
+    y: { type: Number, requred: true }
+
+    // Tuning off auto creation of ID
+}, { _id: false, strict: true });
+
+module.exports = locationSchema;
