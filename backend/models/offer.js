91942a66cc82bff77ca859eb9db9652337a7a7af
Rod_Yon <novakovskiy.ra@outlook.com>
2024-12-29 21:28:25 +0200

    Created 'offer.js' file
    The file 'offer.js' contains schema for offer between two user about specific ad

--- backend/models/offer.js
+++ backend/models/offer.js
@@ -1,5 +1,35 @@
 // Import of necessary modules
 const { Schema, model, Types } = require('mongoose');
-const User = require('./user');
-const Ad = require('./ad');
+const { messageSchema } = require('./message');
+const { statusEnum } = require('./enum');
 
+
+// Schema of offer between two users about specific ad
+const offerSchema = new Schema({
+
+    // ID of user that want to donate an item
+    donorId: { type: Types.ObjectId, ref: 'User', required: true },
+
+    // ID of user that want to receive an item
+    recipientId: { type: Types.ObjectId, ref: 'User', required: true },
+
+    // ID of ad that contains item of interest
+    adId: { type: Types.ObjectId, ref: 'Ad', required: true },
+
+    // Status of the offer (must be one of the values in statusEnum from 'enum.js')
+    status: { type: String, enum: statusEnum, requred: true },
+
+    // List of messages send by both users in chat
+    chat: { type: [messageSchema], default: [], required: true },
+
+    // The date on which users scheduled an appointment
+    appointment: { type: Date},
+
+    // Deate of the offer creation
+    creationDate: { type: Date, default: Date.now, required: true }
+
+}, { collection: 'offers', strict: true });
+
+const Offer = model('Offer', offerSchema);
+
+module.exports = Offer;
