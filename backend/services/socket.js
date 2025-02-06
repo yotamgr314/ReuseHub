2b334ebe3569c89977bfda52086e39aedc32d4de
Yotam Greenstein <yotamgr314@gmail.com>
2025-02-06 02:48:03 +0200

    working partly with double.
    working asycnochircnly messages, but not fetching id needs to redirect on my own to chat/id

--- backend/services/socket.js
+++ backend/services/socket.js
@@ -1,19 +1,19 @@
 // backend/services/socket.js
 module.exports = function (io) {
-  io.on("connection", (socket) => {
-      console.log("🔹 A user connected to WebSocket");
-
-      socket.on("joinChat", (chatId) => {
-          socket.join(chatId);
-          console.log(`🔹 User joined chat: ${chatId}`);
-      });
-
-      socket.on("sendMessage", (data) => {
-          io.to(data.chatId).emit("newMessage", data); // ✅ שליחת ההודעה למשתמשים בצ'אט
-      });
-
-      socket.on("disconnect", () => {
-          console.log("🔹 A user disconnected from WebSocket");
-      });
-  });
-};
+    io.on("connection", (socket) => {
+        console.log("🔹 A user connected to WebSocket");
+  
+        socket.on("joinChat", (chatId) => {
+            socket.join(chatId);
+            console.log(`🔹 User joined chat: ${chatId}`);
+        });
+  
+        socket.on("sendMessage", (data) => {
+            io.to(data.chatId).emit("newMessage", data); // ✅ שליחת ההודעה למשתמשים בצ'אט
+        });
+  
+        socket.on("disconnect", () => {
+            console.log("🔹 A user disconnected from WebSocket");
+        });
+    });
+  };
--- frontend/src/shared/utilis/handleLogout.js
+++ frontend/src/shared/utilis/handleLogout.js
@@ -1,9 +1,8 @@
 // frontend/src/shared/utils/handleLogout.js
 export const handleLogout = (navigate) => {
 
-    localStorage.removeItem('token'); // removes the jwt token from the local storage - other wise it will stay there even if we close the chrome. 
-  
-    navigate('/login'); //  render the login page using React Router navigate()
+  localStorage.removeItem('token'); // removes the jwt token from the local storage - other wise it will stay there even if we close the chrome. 
 
-  };
-  
+  navigate('/login'); //  render the login page using React Router navigate()
+
+};
